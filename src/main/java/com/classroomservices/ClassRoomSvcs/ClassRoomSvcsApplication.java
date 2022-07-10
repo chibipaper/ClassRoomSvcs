172dbd0c202799478affbd11a3541ba582ac18b0
nadine <nadine.heng@ncs.com.sg>
2022-07-10 17:15:00 +0800

    initialise spring

---
+++ src/main/java/com/classroomservices/ClassRoomSvcs/ClassRoomSvcsApplication.java
@@ -0,0 +1,13 @@
+package com.classroomservices.ClassRoomSvcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRoomSvcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRoomSvcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classroomservices/ClassRoomSvcs/ClassRoomSvcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classroomservices.ClassRoomSvcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRoomSvcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
